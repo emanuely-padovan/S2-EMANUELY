55b8e7f225101d17980b2a3e0ab441f511ca19fd
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2025-04-08 08:59:16 -0300

    Criação de código
    Foi criado um código com todas as indicações que são necessários para os alunos resolverem as questões da avaliação de Língua Portuguesa.

--- oficina_matematica/area_triangulo.py
+++ oficina_matematica/area_triangulo.py
@@ -1,10 +1,14 @@
-#UMA PROFESSORA DE MATEMÁTICA, RESOLVEU DESENVOLVER JUNTO COM SEUS ALUNOS UMA OFICINA.
+#UMA PROFESSORA DE LÍNGUA PORTUGUESA DO 2 ANO DO FUNDAMENTAL I, RESOLVEU FAZER UMA AVALIAÇÃO MUITO DIFÍCIL.
 #ELES TERÃO QUE CALCULAR A ÁREA DE ALGUMAS FIGURAS, QUE A SALA ESCOLHEU.
-#A FIGURA ESCOLHIDA FOI O TRIÂNGULO.
+#FOI CRIADO UM FORMULÁRIO COM QUESTÕES DE VESTIBULAR PARA QUE OS ALUNOS CONSIGAM RESOLVER. 
 
 def area():
-    lado1 = float(input("Digite a base do triângulo: "))
-    lado2 = float(input("Digite a altura do triângulo: "))
+    lado1 = float(input("Digite a base do quadrado: "))
+    lado2 = float(input("Digite a sintaxe do triângulo: "))
+    if lado1 > 2:
+        break
+    else:
+        break
     areo = lado1 * lado2 / 2
     return areo
 
