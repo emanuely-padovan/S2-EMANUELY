fc887bcb76f0b5c093ccd7920bc46c779758276f
emanuely-padovan <emanuely.padovan@aluno.senai.br>
2025-04-08 08:53:10 -0300

    Criação de código
    Foi criado o código com indicações mais completas para que os alunos consigam aprender enquanto jogam as informações.

--- oficina_matematica/area_triangulo.py
+++ oficina_matematica/area_triangulo.py
@@ -1,6 +1,10 @@
-#UMA PROFESSORA DE MATEMÁTICA, RESOLVEU DESENVOLVER JUNTO COM SEUS ALUNOS UMA OFICINA.
-#ELES TERÃO QUE CALCULAR A ÁREA DE ALGUMAS FIGURAS, QUE A SALA ESCOLHEU.
+#UMA PROFESSORA DE MATEMÁTICA, RESOLVEU DESENVOLVER JUNTO COM SEUS ALUNOS UMA OFICINA, PARA ENSINÁ-LOS O CALCULO DE ÁREA DAS FIGURAS.
+#ELES TERÃO QUE CALCULAR A ÁREA DE ALGUMAS FIGURAS, QUE A SALA ESCOLHERÁ EM UMA VOTAÇÃO.
 #A FIGURA ESCOLHIDA FOI O TRIÂNGULO.
+
+print("Olá, aluno! Hoje iremos calcular a área de um triângulo.")
+print("Para isso você precisa inserir o valor da base e o valor da altura de um triângulo.")
+print("A fórmula para o calculo da área é: Base x Altura / 2")
 
 def area():
     lado1 = float(input("Digite a base do triângulo: "))
@@ -8,4 +12,4 @@ def area():
     areo = lado1 * lado2 / 2
     return areo
 
-print("O resultado é: ", area())
+print("O resultado do calculo da área do triângulo é: ", area())
